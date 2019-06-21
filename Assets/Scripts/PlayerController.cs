297daa6d3bc7ba00c9d324c32c8d6a0651069842
davidgaroro <davidgaroro@gmail.com>
2019-06-21 19:01:50 +0200

    2.2 Moving the Player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20f;
+
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+
+        m_Rigidbody.AddForce(m_Movement * speed);
+    }
+}
